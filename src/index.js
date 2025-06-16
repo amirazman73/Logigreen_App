66f04a9c2c5655ba21504c69b749ebb118b26ea4
amirazman73 <amirazman73@gmail.com>
2025-06-16 14:49:57 +0200

    Configured to PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Call the registration function to enable the PWA
+serviceWorkerRegistration.register(); 
