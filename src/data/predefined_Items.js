7f7127e7d88e6b443252003d0479234daf3073c1
amirazman73 <amirazman73@gmail.com>
2025-06-18 09:30:26 +0200

    Add items

--- src/data/predefined_Items.js
+++ src/data/predefined_Items.js
@@ -1,8 +1,19 @@
 export const predefinedItems = [
+
+    // Uganda Items
   { name: 'Matoki', source: 'Uganda' },
   { name: 'Apple Banana', source: 'Uganda' },
   { name: 'Sweet Potato', source: 'Uganda' },
-  { name: 'Curry Leaves', source: 'India' },
+  { name: 'Herbs', source: 'Uganda' },
+  { name: 'Hot Pepper', source: 'Uganda' },
+  { name: 'Chili', source: 'Uganda' },
+  { name: 'Garden Eggs', source: 'Uganda' },
+  { name: 'Ginger', source: 'Uganda' },
+  { name: 'Cocoyam', source: 'Uganda' },
+  { name: 'Sugarcane', source: 'Uganda' },
+
+  // India Items
+  { name: 'Herbs', source: 'India' },
   { name: 'Dudhi', source: 'India' },
   // ...add all your other predefined items here
 ];
