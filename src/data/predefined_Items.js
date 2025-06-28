5a79588796783394ec94f30b9b2f1872a77ad83a
amirazman73 <amirazman73@gmail.com>
2025-06-28 09:09:58 +0200

    Items

--- src/data/predefined_Items.js
+++ src/data/predefined_Items.js
@@ -1,19 +1,45 @@
 export const predefinedItems = [
 
     // Uganda Items
-  { name: 'Matoki', source: 'Uganda' },
+  { name: 'Matoke', source: 'Uganda' },
   { name: 'Apple Banana', source: 'Uganda' },
   { name: 'Sweet Potato', source: 'Uganda' },
   { name: 'Herbs', source: 'Uganda' },
   { name: 'Hot Pepper', source: 'Uganda' },
-  { name: 'Chili', source: 'Uganda' },
+  { name: 'Bird Eye Chili', source: 'Uganda' },
   { name: 'Garden Eggs', source: 'Uganda' },
   { name: 'Ginger', source: 'Uganda' },
   { name: 'Cocoyam', source: 'Uganda' },
   { name: 'Sugarcane', source: 'Uganda' },
+  { name: 'Avocado', source: 'Uganda' },
+  { name: 'Peanuts', source: 'Uganda' },
+  { name: 'Herbs leaves pp 40pkts', source: 'Uganda' },
 
   // India Items
+  { name: 'Red Pumpkin (8kg)', source: 'India' },
+  { name: 'Shallot Onion', source: 'India' },
+  { name: 'Kola (8kg)', source: 'India' },
+  { name: 'Drumstick (5kg)', source: 'India' },
+  { name: 'Thai Guava (5kg)', source: 'India' },
+  { name: 'Yellow Haldi', source: 'India' },
+  { name: 'Arvi', source: 'India' },
   { name: 'Herbs', source: 'India' },
   { name: 'Dudhi', source: 'India' },
+  { name: 'Guwar', source: 'India' },
+  { name: 'Shallot Onion (5kg)', source: 'India' },
+  { namea: 'Amla (5kg)', source: 'India' },
+  { name: 'Suran (5kg)', source: 'India' },
+  { name: 'Yellow Haldi (5kg)', source: 'India' },
+  { name: 'Arvi (5kg)', source: 'India' },
+  { name: 'Vallerika (5kg)', source: 'India' },
+  { name: 'Chikoo (5kg)', source: 'India' },
+  { name: 'Bombay Onion (5kg)', source: 'India' },
+  { name: 'Kola (5kg)', source: 'India' },
+
+
+  { name: 'Tindori', source: 'India' },
+  { name: 'V-papdi', source: 'India' },
+  { name: 'B-papdi', source: 'India' },
+  { name: 'Long Beans', source: 'India' },
   // ...add all your other predefined items here
 ];
